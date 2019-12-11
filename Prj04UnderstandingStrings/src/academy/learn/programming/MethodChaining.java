14155b96650dd68f954579414d6bfc9ebc78d8cc
Denise Nanni <denise_n@hotmail.it>
2019-12-11 20:54:43 +0100

    MethodChaining added

--- Prj04UnderstandingStrings/src/academy/learn/programming/MethodChaining.java
+++ Prj04UnderstandingStrings/src/academy/learn/programming/MethodChaining.java
@@ -3,5 +3,9 @@ package academy.learn.programming;
 public class MethodChaining {
     public static void main(String[] args) {
 
+        String str = "Java    ";
+        String strimmed = str.trim().toUpperCase();
+        System.out.println(strimmed);
+
     }
 }
