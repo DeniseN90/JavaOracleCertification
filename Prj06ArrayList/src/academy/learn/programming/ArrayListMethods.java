e6084569e73b779a126eccfa51f5bce52f8c0f82
Denise Nanni <denise_n@hotmail.it>
2019-12-30 10:06:56 +0100

    Arraylist

---
+++ Prj06ArrayList/src/academy/learn/programming/ArrayListMethods.java
@@ -0,0 +1,46 @@
+package academy.learn.programming;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by catenate on 30/12/2019.
+ */
+public class ArrayListMethods {
+    public static void main(String[] args) {
+       // List list = new ArrayList<>();   same as List<Object>
+        List<Object> list = new ArrayList<>();
+        list.add("dog");
+        list.add(5);
+        System.out.println(list);
+
+        // add
+        List<String> pets = new ArrayList<>(); // the compiler knows that just strings go here
+        pets.add("dog");
+        pets.add(0, "cat");
+        System.out.println(pets);
+//        pets.add(6); this list can hold just strings
+        pets.add(1, "parrot");
+        System.out.println(pets);
+        pets.add(0, "fish");
+        System.out.println(pets);
+
+        // remove
+
+        pets.remove("parrot");
+        System.out.println(pets  );
+        pets.remove(2);
+        System.out.println(pets);
+
+
+        // set
+
+        pets.set(0, "sea lion");  // pets[0] = "sea lion" => in case of arrays
+        System.out.println(pets);
+
+//        pets.set(6, "dodo");  indexOutOfBoundException
+
+
+
+    }
+}
---
+++ Prj06ArrayList/src/academy/learn/programming/UnderstandingArrayList.java
@@ -0,0 +1,19 @@
+package academy.learn.programming;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by catenate on 30/12/2019.
+ */
+public class UnderstandingArrayList {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>(); // using interface as type
+
+        List<String> listTwo = new ArrayList<>(20);
+
+
+
+    }
+}
