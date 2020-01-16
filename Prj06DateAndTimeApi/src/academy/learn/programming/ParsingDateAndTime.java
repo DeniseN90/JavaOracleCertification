1692fa74804536b868f2a7a21bb84ab82fc2e0a0
Denise Nanni <denise_n@hotmail.it>
2020-01-16 23:16:26 +0100

    DateTimeFormatter

--- Prj06DateAndTimeApi/src/academy/learn/programming/ParsingDateAndTime.java
+++ Prj06DateAndTimeApi/src/academy/learn/programming/ParsingDateAndTime.java
@@ -1,7 +1,16 @@
 package academy.learn.programming;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class ParsingDateAndTime {
     public static void main(String[] args) {
-        
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("MM dd yyyy");
+        LocalDate date = LocalDate.parse("01 02 2015", f);
+        LocalTime time = LocalTime.parse("11:22");
+        System.out.println(date); // 2015-01-02
+        System.out.println(time); // 11:22
+
     }
 }
