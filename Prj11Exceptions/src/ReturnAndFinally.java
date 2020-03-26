7a9c7c1f85e0b3e9fc9874e63f7ad9d26b184d75
Denise Nanni <denise_n@hotmail.it>
2020-03-26 08:46:10 +0100

    modified exceptions

--- Prj11Exceptions/src/ReturnAndFinally.java
+++ Prj11Exceptions/src/ReturnAndFinally.java
@@ -6,6 +6,7 @@ public class ReturnAndFinally {
     public static void main(String[] args) {
 
         System.out.println(calculate());
+        System.out.println(say());
     }
 
     public static int calculate(){
@@ -19,6 +20,21 @@ public class ReturnAndFinally {
         } finally {
             System.out.println("finally");
         //    return 0;
+        }
+    }
+
+
+
+    public static String say(){
+        try {
+            System.out.println("gonna throw");
+            throw new RuntimeException();
+          //  System.out.println("threw..");  unreachable statement
+
+        } finally {
+
+            System.out.println("done");
+            // return "done"; with the return statement the exception is not thrown, even thogh there isn't a catch statement
         }
     }
 }
